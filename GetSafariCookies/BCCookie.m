fb7d6f0b2fd4df0ba803becccb7c20b2785b7e96
Jeremy Beker <gothmog@confusticate.com>
2026-05-29 08:55:19 -0400

    Implement BCCookie netscape line formatting

--- GetSafariCookies/BCCookie.m
+++ GetSafariCookies/BCCookie.m
@@ -1,5 +1,14 @@
 #import "BCCookie.h"
 
 @implementation BCCookie
-- (NSString *)netscapeLine { return @""; }
+- (NSString *)netscapeLine {
+    NSString *domainField = self.httpOnly
+        ? [@"#HttpOnly_" stringByAppendingString:self.domain]
+        : self.domain;
+    NSString *includeSub = [self.domain hasPrefix:@"."] ? @"TRUE" : @"FALSE";
+    NSString *secureField = self.secure ? @"TRUE" : @"FALSE";
+    return [NSString stringWithFormat:@"%@\t%@\t%@\t%@\t%ld\t%@\t%@\n",
+            domainField, includeSub, self.path, secureField,
+            self.expiresUnix, self.name, self.value];
+}
 @end
